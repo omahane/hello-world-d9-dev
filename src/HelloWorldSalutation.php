077616ce9962699d836178b92fd2a42c43c24fe4
Christian Burk <omahane@gmail.com>
2021-09-14 23:08:11 -0500

    Swapped afternoon and morning to test

--- src/HelloWorldSalutation.php
+++ src/HelloWorldSalutation.php
@@ -16,12 +16,13 @@ class HelloWorldSalutation {
    */
   public function getSalutation() {
     $time = new \DateTime();
-    if ((int) $time->format('G') >= 00 && (int) $time->format('G') < 12) {
-      return $this->t('Good morning world');
-    }
     if ((int) $time->format('G') >= 12 && (int) $time->format('G') < 18) {
       return $this->t('Good afternoon world');
     }
+    if ((int) $time->format('G') >= 00 && (int) $time->format('G') < 12) {
+      return $this->t('Good morning world');
+    }
+
     if ((int) $time->format('G') >= 18) {
       return $this->t('Good evening world');
     }
